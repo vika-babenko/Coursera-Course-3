e3dbc3fe4975d25ab5131252734872fa4d74a54a
vika-babenko <vika.babenko.18.09.02@gmail.com>
2022-12-07 14:11:00 +0200

    add answers on course in public rep

---
+++ Week 3/Mac-Programming-Assignment-3-Materials/autograder materials/ProgrammingAssignment3/ProgrammingAssignment3/Mover.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Mover the game object back and forth
+/// </summary>	
+public class Mover : MonoBehaviour
+{
+    #region Fields
+
+    // don't change any of these fields!
+
+    const float MoveAmountPerSecond = 5;
+    const float TimerDuration = 1;
+
+    // direction-changing support
+    Timer directionTimer;
+    int directionMultiplier = 1;
+
+    #endregion
+
+    #region Properties - for autograder use only!
+
+    /// <summary>
+    /// Gets and sets the position of the game object
+    /// </summary>
+    public Vector3 Position
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+
+    /// <summary>
+    /// Gets the direction timer so the autograder can
+    /// update the timer every frame
+    /// </summary>
+    public Timer DirectionTimer
+    {
+        get { return directionTimer; }
+    }
+
+    #endregion
+
+    #region Unity methods
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>	
+    public void Start()
+    {
+        // set position of game object for autograder
+        transform.position = new Vector3(-2.5f, 0, 0);
+
+        // add and run the timer
+        directionTimer = gameObject.AddComponent<Timer>();
+        DirectionTimer.Duration = TimerDuration;
+        DirectionTimer.Run();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>	
+    public void Update()
+    {
+        // change direction as appropriate
+        // var timer = GetComponent<Timer>();
+
+        if (DirectionTimer.Finished)
+        {
+            DirectionTimer.Stop();
+            directionMultiplier *= (-1);
+            DirectionTimer.Run();
+        }
+
+        // move game object
+        Position += new Vector3(directionMultiplier * MoveAmountPerSecond * Time.deltaTime, 0f, 0f);
+    }
+
+    #endregion
+}
